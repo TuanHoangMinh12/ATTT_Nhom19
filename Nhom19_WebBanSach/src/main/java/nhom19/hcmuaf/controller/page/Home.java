28a0a047a4e6a4d7edce30cb191aa8c3947d6bbd
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2024-12-04 23:49:16 +0700

    add code

---
+++ Nhom19_WebBanSach/src/main/java/nhom19/hcmuaf/controller/page/Home.java
@@ -0,0 +1,56 @@
+package nhom19.hcmuaf.controller.page;
+
+import nhom19.hcmuaf.beans.Products;
+import nhom19.hcmuaf.beans.Users;
+import nhom19.hcmuaf.beans.cart.Cart;
+import nhom19.hcmuaf.beans.cart.UserCart;
+import nhom19.hcmuaf.services.ProductService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+import nhom19.hcmuaf.util.MyUtils;
+
+@WebServlet(urlPatterns = {"/page/home"})
+public class Home extends HttpServlet {
+
+  // oh no
+  private static final long serialVersionUID = 1L;
+
+  public Home() {
+    super();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+    // Dẫn đến đường link trang chủ hiển thị ra 8 sản phẩm
+    List<Products> products = ProductService.getInstance().getProduct();
+    request.setAttribute("listProducts", products);
+
+    HttpSession session = request.getSession();
+    //create cart if not exist
+    Cart cart;
+    Users user = MyUtils.getLoginedUser(session);
+    if (MyUtils.getLoginedUser(session) != null && UserCart.getUserCart(user.getId()) != null) {
+      cart = UserCart.getUserCart(user.getId());
+      MyUtils.storeCart(session, cart);
+    } else if (MyUtils.getCart(session) == null) {
+      cart = new Cart();
+      MyUtils.storeCart(session, cart);
+    }
+
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/index.jsp");
+    dispatcher.forward(request, response);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+  }
+
+}
