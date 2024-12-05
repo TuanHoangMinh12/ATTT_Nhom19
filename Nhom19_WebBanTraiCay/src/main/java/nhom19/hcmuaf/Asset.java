46bbd8ebdd0d959c42d6d2e2470d1c38e61ca8c1
Pham Hoang Tien <21130562@st.hcmuaf.edu.vn>
2024-12-05 23:04:55 +0700

    Initial commit (temporary)

---
+++ Nhom19_WebBanTraiCay/src/main/java/nhom19/hcmuaf/Asset.java
@@ -0,0 +1,8 @@
+package nhom19.hcmuaf;
+
+public class Asset {
+    public static String baseUrl= "http://localhost:8080/";
+    public static String url(String s) {
+        return baseUrl+s;
+    }
+}
---
+++ Nhom19_WebBanTraiCay/src/main/java/nhom19/hcmuaf/dao/RoleDAO.java
@@ -0,0 +1,9 @@
+package nhom19.hcmuaf.dao;
+
+import nhom19.hcmuaf.beans.Role;
+
+import java.util.List;
+
+public interface RoleDAO {
+    public List<Role> getAllRoles();
+}
