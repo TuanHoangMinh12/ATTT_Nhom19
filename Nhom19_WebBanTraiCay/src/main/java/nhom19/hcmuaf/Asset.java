14b21b919cba9b4d6576128ef69bed757db85bda
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2024-12-07 10:12:57 +0700

    xoa thu muc

--- Nhom19_WebBanTraiCay/src/main/java/nhom19/hcmuaf/Asset.java
+++
@@ -1,8 +0,0 @@
-package nhom19.hcmuaf;
-
-public class Asset {
-    public static String baseUrl= "http://localhost:8080/";
-    public static String url(String s) {
-        return baseUrl+s;
-    }
-}
